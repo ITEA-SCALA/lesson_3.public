879d230c75f38598fd70967e2958d2dfe12d258d
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2021-10-05 12:07:22 +0300

    #master  lesson_3: task2

--- src/main/scala/com/itea/task1/SimpleApp.scala
+++ src/main/scala/com/itea/task1/SimpleApp.scala
@@ -4,10 +4,17 @@ import com.typesafe.config.ConfigFactory
 
 object SimpleApp extends App {
 
-  val conf = ConfigFactory.load()
+  //
+  val conf = ConfigFactory.load() // "application"
+//  val conf = ConfigFactory.load("application2.conf") // system properties: No configuration setting found for key 'simple-app'
 
   println("simple-app.answer: " + conf.getString("simple-app.answer"))
   println("simple-lib.foo: " + conf.getString("simple-lib.foo"))
   println("simple-lib.whatever: " + conf.getString("simple-lib.whatever"))
+
+  //
+  case class ConfigDb(login: String, password: String, url: String)
+  val configDb = conf.getAnyRef("config.db")
+  println("ConfigDb: " + configDb)
 
 }
