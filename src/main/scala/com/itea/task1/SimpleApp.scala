27d64246340dd697a760bf2ac4b1dc8aa756a55d
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2021-10-05 14:17:08 +0300

    #master  lesson_3: task2

--- src/main/scala/com/itea/task1/SimpleApp.scala
+++ src/main/scala/com/itea/task1/SimpleApp.scala
@@ -1,6 +1,6 @@
 package com.itea.task1
 
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 
 object SimpleApp extends App {
 
@@ -20,13 +20,19 @@ object SimpleApp extends App {
    * # 2.
    * TODO Exception in thread "main" scala.MatchError: {password=mysecretpassword, login=myuser, url=jdbc://mysql.....} (of class java.util.HashMap)
    */
-  def mapper(dbConfig: AnyRef) = dbConfig match {
-    case config @ DbConfig(_,_,_) => config
-    case _ => DbConfig
-  }
-
+//  def mapper(dbConfig: AnyRef): DbConfig = dbConfig match {
+//    case config: DbConfig => DbConfig(login=config.login, password=config.password, url=config.url)
+////    case _ => DbConfig
+//  }
+//
 //  val dbConfig2: DbConfig = mapper( conf.getAnyRef("config.db") )
-//  println("db.login = " + dbConfig2.login)
+////  println("db.login = " + dbConfig2.login)
 
+//  val url = conf.getConfig("config.db").getString("url")
+//  println("url: " + url)
+  //
+  val dbConfig2: Config = conf.getConfig("config.db")
+  val url = dbConfig2.getString("url")
+  println("url: " + url)
 
 }
